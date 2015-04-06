d8011b4447af9f8adb4730953817737f0bf4f352
JulianOrinyol <julianorinyol@gmail.com>
2015-04-06 12:33:24 -0700

    Wrote player object

---
+++ jsBettingGame.js
@@ -0,0 +1,24 @@
+(function(){
+
+var log = console.log.bind(console);
+player1 = {
+  firstname: "Benny",
+  lastname: "Templeton",
+  nickname: "mad-dawg",
+  cash: 100
+}
+
+
+var game = {
+
+
+  
+}
+
+
+
+
+
+
+
+})()
